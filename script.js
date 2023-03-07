4beca0217bbca76be58746083133f4897e97c1fd
Annazio <124679541+Annazio@users.noreply.github.com>
2023-03-07 17:18:42 +0100

    Add files via upload
    Java Script 1

---
+++ script.js
@@ -0,0 +1,32 @@
+//Задание 1
+const number1 = prompt ("Введите число")
+  console.log(number1 * 0.10)
+
+
+// Задание 2
+ const number2 = 40
+ const number3 = 35
+ if(number2 < number3) {
+     console.log(number2)
+ } else {
+     console.log(number3)
+ }
+
+//Задание 3
+const number4 = prompt ("Введите число")
+  if(number4 < 100) {
+      console.log("Число меньше 100")
+  } else if(number4 > 100) {
+      console.log("Число больше 100")
+  } else {
+      console.log("Число равно 100")
+  }
+
+ // Задание 4
+  const Name = prompt ("Введите Ваше имя")
+  const Age = prompt ("Введите Ваш возраст")
+  if(Age >= 18) {
+      console.log("Hello" + Name)
+  } else {
+      console.log("Hi" + Name)
+  }
